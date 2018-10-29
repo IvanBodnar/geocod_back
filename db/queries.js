5bd4fd6b17a5ab0607e8c2726de4153cb7d1ee29
ivanbodnar <ibodnar@outlook.com>
2018-10-29 17:23:10 -0300

    Added queries.js -> fn executeQuery

---
+++ db/queries.js
@@ -0,0 +1,12 @@
+const sequelize = require('./connection');
+
+
+const executeQuery = async (query, ...args) => {
+    const s = await sequelize.query(
+        query + ' AS resultado;',
+        { replacements: args, type: sequelize.QueryTypes.SELECT }
+    );
+    return s[0].resultado;
+}
+
+module.exports = executeQuery;
