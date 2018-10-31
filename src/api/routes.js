11373cc5992fb6e39789a5f44f75a460a0b429b5
ivanbodnar <ibodnar@outlook.com>
2018-10-31 16:20:24 -0300

    Added routes /streets and /street/:name

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const streetFactory = require('../geocod/street_model');
+const getStreets = require('../geocod/streets');
 
 const router = express.Router();
 
+
 router.get('/streets', (req, res, next) => {
-    res.send('test');
+    const streets = getStreets();
+    streets
+        .then(streets => res.send(streets));
 });
+
+router.get('/street/:name', (req, res, next) => {
+    const street = streetFactory(req.params.name);
+    street
+        .then(street => res.send(street));
+})
 
 module.exports = router;
