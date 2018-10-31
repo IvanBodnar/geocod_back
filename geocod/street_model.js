1910efd0c211330ff1a3c5b2f324efdee36ad0f5
ivanbodnar <ibodnar@outlook.com>
2018-10-31 10:33:44 -0300

    Street.init and Street.streetNumber -> throw error <-> return

--- geocod/street_model.js
+++ geocod/street_model.js
@@ -9,10 +9,11 @@ class Street {
 
     static async init(name) {
         const exists = await executeQuery(`SELECT existe_calle(?)`, name);
-        if (exists) {
-            return new Street(name);
+        if (!exists) {
+            throw new Error('Street does not exists.');
         }
-        throw new Error('Street does not exists.');
+        
+        return new Street(name);
     }
 
     async streetNumber(number) {
@@ -21,6 +22,10 @@ class Street {
             this.name,
             number
         )
+        if (!coords) {
+            throw new Error('Street number does not exists.');
+        }
+        
         return coords;
     }
 }
@@ -30,7 +35,7 @@ async function res() {
     // const s = new Street('santander');
     try {
         const s = await Street.init('santander');
-        console.log(await s.streetNumber('5400'));
+        console.log( await s.streetNumber('5400'));
     }
     catch(error) {
         console.log(error);
