c300bff8e69063d617bf1cb2dd4e619a95edfb25
ivanbodnar <ibodnar@outlook.com>
2018-10-31 16:39:03 -0300

    catch added in routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -8,13 +8,15 @@ const router = express.Router();
 router.get('/streets', (req, res, next) => {
     const streets = getStreets();
     streets
-        .then(streets => res.send(streets));
+        .then(streets => res.send(streets))
+        .catch(error => console.log(error));
 });
 
 router.get('/street/:name', (req, res, next) => {
     const street = streetFactory(req.params.name);
     street
-        .then(street => res.send(street));
-})
+        .then(street => res.send(street))
+        .catch(error => console.log(error));
+});
 
 module.exports = router;
