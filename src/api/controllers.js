d7b993b42544c001deed209bc381b57557d310e1
ivanbodnar <ibodnar@outlook.com>
2018-10-31 19:42:25 -0300

    Refactor: logic moved from routes.js to controllers.js

---
+++ src/api/controllers.js
@@ -0,0 +1,35 @@
+const getStreets = require('../geocod/streets');
+const streetFactory = require('../geocod/street_model');
+const getIntersection = require('../geocod/intersection');
+
+
+const streets = (req, res, next) => {
+    const streets = getStreets();
+    streets
+        .then(streets => res.send(streets))
+        .catch(error => console.log(error));
+};
+
+const street = (req, res, next) => {
+    const street = streetFactory(req.params.name);
+    street
+        .then(street => res.send(street))
+        .catch(error => console.log(error));
+};
+
+const intersection = async (req, res, next) => {
+    try {
+        const street1 = await streetFactory(req.query.street1);
+        const street2 = await streetFactory(req.query.street2);
+        const intersection = await getIntersection(street1.name, street2.name);
+        res.send({ intersection: intersection });
+    } catch (error) {
+        res.send({ error: error.message });
+    }
+};
+
+module.exports = {
+    streets: streets,
+    street: street,
+    intersection: intersection
+};
--- src/api/routes.js
+++ src/api/routes.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const streetFactory = require('../geocod/street_model');
-const getStreets = require('../geocod/streets');
+const controllers = require('./controllers');
 
 const router = express.Router();
 
 
-router.get('/streets', (req, res, next) => {
-    const streets = getStreets();
-    streets
-        .then(streets => res.send(streets))
-        .catch(error => console.log(error));
-});
+router.get('/streets', controllers.streets);
 
-router.get('/street/:name', (req, res, next) => {
-    const street = streetFactory(req.params.name);
-    street
-        .then(street => res.send(street))
-        .catch(error => console.log(error));
-});
+router.get('/street/:name', controllers.street);
+
+router.get('/intersection', controllers.intersection);
 
 module.exports = router;
