540724146331bd42366c1041dc397024a855ccb9
ivanbodnar <ibodnar@outlook.com>
2018-10-31 13:57:50 -0300

    main.js added - test route added

---
+++ src/api/main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded( {extended: false} ));
+
+app.use('/', routes);
+
+app.listen(8080);
