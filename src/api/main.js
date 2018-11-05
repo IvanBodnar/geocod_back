6fc940bb0690da48a88f3e1923aef3856b713f02
ivanbodnar <ibodnar@outlook.com>
2018-11-05 10:19:07 -0300

    Middleware to add cors headers

--- src/api/main.js
+++ src/api/main.js
@@ -7,6 +7,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/', routes);
 
 app.listen(3000);
